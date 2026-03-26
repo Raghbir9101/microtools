96881583a2082986792336ac5561ba349fd4f7fd
Raghbir Singh <rkgaming0203@gmail.com>
2026-03-27 02:50:56 +0530

    added bg remove

---
+++ .next/server/app/resize-image-100kb-railway.segments/_tree.segment.rsc
@@ -0,0 +1,2 @@
+:HL["/_next/static/chunks/0w0vhn7tulgbw.css","style"]
+0:{"tree":{"name":"","param":null,"prefetchHints":16,"slots":{"children":{"name":"resize-image-100kb-railway","param":null,"prefetchHints":0,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":0,"slots":null}}}}},"staleTime":300,"buildId":"X9RK343RPxAVdNzvpNSY4"}
---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,21 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/03~yq9q893hmn.js"
+  ],
+  "lowPriorityFiles": [
+    "static/X9RK343RPxAVdNzvpNSY4/_buildManifest.js",
+    "static/X9RK343RPxAVdNzvpNSY4/_ssgManifest.js",
+    "static/X9RK343RPxAVdNzvpNSY4/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/0pvne~44e3fql.js",
+    "static/chunks/0pqt~8bl3ukh4.js",
+    "static/chunks/00ilazh50sdzh.js",
+    "static/chunks/0e-wrhqh4h3r5.js",
+    "static/chunks/turbopack-0~cs.oso6got5.js"
+  ]
+};
