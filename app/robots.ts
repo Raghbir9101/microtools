d7ed18f162a0109cf5679bfe27c5b892b499be9e
Raghbir Singh <rkgaming0203@gmail.com>
2026-03-27 14:08:02 +0530

    added bg remove

--- app/robots.ts
+++ app/robots.ts
@@ -6,10 +6,9 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/_next/'],
+        disallow: ['/api/'],
       },
     ],
     sitemap: 'https://tools.draftly.co.in/sitemap.xml',
-    host: 'https://tools.draftly.co.in',
   };
 }
