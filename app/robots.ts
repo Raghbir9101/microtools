e6d50c852e58e5443da1c91bfc69cc466111ca28
Raghbir Singh <rkgaming0203@gmail.com>
2026-03-27 18:36:52 +0530

    fix: rename sitemap to sitemap_tools.xml to force fresh Google fetch

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/api/'],
       },
     ],
-    sitemap: 'https://tools.draftly.co.in/sitemap.xml',
+    sitemap: 'https://tools.draftly.co.in/sitemap_tools.xml',
   };
 }
